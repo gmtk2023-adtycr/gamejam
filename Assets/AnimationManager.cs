9bada7390ef660894238f3e94a5c947367a0ce9e
dimesto52 <98359317+dimesto52@users.noreply.github.com>
2023-07-09 19:39:08 +0200

    togjgh

--- Assets/AnimationManager.cs
+++ Assets/AnimationManager.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
-using UnityEditor.Animations;
 
 public class AnimationManager : MonoBehaviour
 {
