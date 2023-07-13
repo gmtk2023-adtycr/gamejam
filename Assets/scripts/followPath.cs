33ea4bbd9f93e81266278fc45148b22010598803
Valentin Vermandé <valentin.vermande@telecomnancy.net>
2023-07-13 09:24:51 +0200

    Added waitingTime to enemies pathpoints

--- Assets/scripts/followPath.cs
+++ Assets/scripts/followPath.cs
@@ -6,9 +6,11 @@ public class followPath : MonoBehaviour
 {
     public pathPoint point;
     public float speed;
+    
 
-    Rigidbody2D rb;
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    public bool waiting = false;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,17 +20,30 @@ public class followPath : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (point != null)
-        {
-            if (Vector3.Distance(transform.position, point.transform.position) < 0.3f)
-                point = point.nextPoint;
-            GoToNextPoint();
+        if (waiting || point == null)
+            return;
+        
+        if (Vector3.Distance(transform.position, point.transform.position) < 0.3f){
+            if (point.waitingTime > 0f){
+                waiting = true;
+                rb.velocity = Vector2.zero;
+                Invoke(nameof(StopWaiting), point.waitingTime);
+            }
+            point = point.nextPoint;
         }
+        else
+            GoToNextPoint();
+
     }
 
     private void GoToNextPoint()
     {
         Vector3 direction = Vector3.Normalize(point.transform.position - transform.position);
         rb.velocity = direction * speed;
+    }
+
+    private void StopWaiting(){
+        waiting = false;
+        GoToNextPoint();
     }
 }
