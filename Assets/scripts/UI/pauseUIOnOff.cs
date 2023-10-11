6390d4ed95b49f0542f9d2ddeba84446032b3ab3
BRS0T <spam.brst@gmail.com>
2023-10-11 19:45:16 +0200

    muffled sound menu pause et essai transition fantomes

--- Assets/scripts/UI/pauseUIOnOff.cs
+++ Assets/scripts/UI/pauseUIOnOff.cs
@@ -2,10 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
 
 public class pauseUIOnOff : MonoBehaviour
 {
     public GameObject menu;
+    [SerializeField] private AudioMixer myAudioMixer;
+    [Range(200,8000)][SerializeField] private float muffled = 1800;
+    [Range(10000,22000)][SerializeField] private float not_muffled = 22000;
+
+
+    [Range(200,8000)][SerializeField] private float highpassed = 100;
+    [Range(10000,22000)][SerializeField] private float not_highpassed = 10;
+
+
+        private bool isPaused = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,12 +35,21 @@ public class pauseUIOnOff : MonoBehaviour
 
     public void toggleMenu()
     {
-        menu.SetActive(!menu.activeSelf);
+        isPaused = !isPaused;
+        menu.SetActive(isPaused);
 
-        if(Time.timeScale == 0)
-            Time.timeScale = 1;
-        else
+        if (isPaused)
+        {
             Time.timeScale = 0;
+            myAudioMixer.SetFloat("MUFFLED IN MENU", muffled);
+            myAudioMixer.SetFloat("HIGHPASS IN MENU", highpassed);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            myAudioMixer.SetFloat("MUFFLED IN MENU", not_muffled);
+            myAudioMixer.SetFloat("HIGHPASS IN MENU", not_highpassed);
+        }
     // }
     // public void returnMenu()
     // {
@@ -38,4 +59,10 @@ public class pauseUIOnOff : MonoBehaviour
     // public void QuitGame(){
     //     Application.Quit();
     }
+
+    public void ExitMenu()
+    {
+
+    myAudioMixer.SetFloat("MUFFLED IN MENU", not_muffled);
+    myAudioMixer.SetFloat("HIGHPASS IN MENU", not_highpassed);    }
 }
