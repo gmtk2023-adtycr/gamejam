dded6bdca579fc49655f3f0e4e71cbc77e4a4736
Valentin Vermandé <valentin.vermande@telecomnancy.net>
2024-01-07 17:02:37 +0100

    Contraindre la caméra aux bords de la maison

--- Assets/scripts/CameraMovement.cs
+++ Assets/scripts/CameraMovement.cs
@@ -5,32 +5,35 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Transform target;
     public float smoothing;
     private Camera _camera;
+    private Transform _target;
 
     private void Start(){
         _camera = GetComponent<Camera>();
+        _target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
-    void LateUpdate()
-    {
-        if(transform.position != target.position){
-            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
-            var grid = Grid.GetGridForPos(targetPosition);
-            var minPos = grid.WalkableLowerLeftCorner;
-            var offset = new Vector3(_camera.orthographicSize * _camera.aspect, _camera.orthographicSize);
-            minPos += offset;
-            var maxPos = grid.WalkableUpperRightCorner;
-            maxPos -= offset;
+    void FixedUpdate(){
+        if (transform.position == _target.position) return;
+        
+        Vector3 targetPosition = new Vector3(_target.position.x, _target.position.y, transform.position.z);
+        var grid = Grid.GetGridForPos(targetPosition);
+        if(grid == null)
+            return;
+        var minPos = grid.WalkableLowerLeftCorner;
+        var offset = new Vector3(_camera.orthographicSize * _camera.aspect, _camera.orthographicSize) 
+                     + Vector3.left + Vector3.down;
+        minPos += offset;
+        var maxPos = grid.WalkableUpperRightCorner;
+        maxPos -= offset;
 
-            targetPosition.x = Mathf.Clamp(targetPosition.x, minPos.x, maxPos.x);
-            targetPosition.y = Mathf.Clamp(targetPosition.y, minPos.y, maxPos.y);
+        targetPosition.x = Mathf.Clamp(targetPosition.x, minPos.x, maxPos.x);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, minPos.y, maxPos.y);
 
-            transform.position = Vector3.Lerp(transform.position, targetPosition, .5f);
-        }
-    
+        transform.position = Vector3.Lerp(transform.position, targetPosition, .1f);
+
     }
     
 }
